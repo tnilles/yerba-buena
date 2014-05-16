5164e3e03be9691a09149ea49c5650b4409e01b2
Thierry Nilles <eagle1792@gmail.com>
2014-05-16 21:13:28 +0200

    Fixed bug with the signin The selectors didn't match the actual html

--- client/views/accounts/signup.js
+++ client/views/accounts/signup.js
@@ -1,8 +1,8 @@
 Template.signup.events({
     'submit #register-form' : function(e, t) {
         e.preventDefault();
-        var email = t.find('#account-email').value,
-            password = t.find('#account-password').value;
+        var email = t.find('#email').value,
+            password = t.find('#password').value;
 
         // Trim and validate the input
 
