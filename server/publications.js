93080a8d3cd6ac67ca5f1b166d3bcd7a4b1454c7
Thierry Nilles <eagle1792@gmail.com>
2014-05-20 03:07:42 +0200

    Forgot to add the persons publication

--- server/publications.js
+++ server/publications.js
@@ -5,3 +5,7 @@ Meteor.publish('openings', function(limit) {
 Meteor.publish('companies', function(limit) {
     return Companies.find({userId: this.userId}, {sort: {submitted: -1}});
 });
+
+Meteor.publish('persons', function(limit) {
+    return Persons.find({userId: this.userId}, {sort: {submitted: -1}});
+});
